20f271a70bde4b367c7854104f9080f45c41038b
Eli Huebner <elimhuebner@gmail.com>
2020-09-23 00:01:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,8 @@ class Triangle
   end
   
   def kind
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a + @b < @c || @b + @c < @a || @a + @c < @b
+      
     
     elsif @a == @b && @b == @c
       :equilateral
